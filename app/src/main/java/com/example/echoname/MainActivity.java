9986ae14b1811b494d23d16a8b4cadd8de51cb48
Tyler Lyczak <tyler.m.lyczak@gmail.com>
2019-11-19 14:49:55 -0600

    Added a textview that shows what you typed in

--- app/src/main/java/com/example/echoname/MainActivity.java
+++ app/src/main/java/com/example/echoname/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         EditText inputText = (EditText)findViewById(R.id.inputText);
 
         Toast.makeText(this, inputText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        TextView changeText = (TextView)findViewById(R.id.echoChangeText);
+        String constant = "You Said: ";
+        changeText.setText(constant+inputText.getText().toString());
     }
 
     @Override
