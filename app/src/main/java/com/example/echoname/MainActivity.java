3fc2cf09c3fe8228abce86c4be92206f97b63dd6
Tyler Lyczak <tyler.m.lyczak@gmail.com>
2019-11-19 14:38:21 -0600

    Added functionality

--- app/src/main/java/com/example/echoname/MainActivity.java
+++ app/src/main/java/com/example/echoname/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void submitClick (View view) {
 
-        //EditText inputText = (EditText)findViewById(R.id.inputText).getContext().toString();
+        EditText inputText = (EditText)findViewById(R.id.inputText);
+
+        Toast.makeText(this, inputText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
